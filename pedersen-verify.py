ab30531d21f7cba5177712f41818b8be826da974
kanishka5 <kanishkan14@gmail.com>
2020-11-13 20:09:04 +0530

    pederson modified

--- pedersen-verify.py
+++ pedersen-verify.py
@@ -6,32 +6,6 @@ from Crypto.Util import number
 global g, h, p, q
 
 
-# check if no. is prime
-def isPrime(n):
-    if (n <= 1):
-        return False
-    if (n <= 3):
-        return True
-    if (n % 2 == 0 or n % 3 == 0):
-        return False
-    i = 5
-    while(i * i <= n):
-        if (n % i == 0 or n % (i + 2) == 0):
-            return False
-        i = i + 6
-
-    return True
-
-
-# prime nos generator
-def getPrimes(n):
-    k = number.getPrime(n)  # n-bit random prime no
-    s = k-1
-    while((k-1) % s != 0 or not isPrime(s)):
-        s -= 1
-    return k, s
-
-
 # power in O(|y|)
 def power(x, y, z):
     res = 1
@@ -83,7 +57,7 @@ def getHval(g, n):
 def coeff(k, secret):
     coeff = [secret]
     for i in range(k-1):
-        coeff.append(random.randrange(0, q))
+        coeff.append(random.getrandbits(128))
     return coeff
 
 
@@ -97,11 +71,9 @@ def calcY(x, coeffs, k):
 
 # split secret between shareholders
 def generateShares(coeffsS, coeffsT, shareno, k):
-    shares = []
-    for x in range(1, shareno+1):
-        shares.append([calcY(x, coeffsS, k), calcY(x, coeffsT, k)])
-
-    return shares
+    # only 1 share genrated
+    x = random.getrandbits(128)
+    return calcY(x, coeffsS, k), calcY(x, coeffsT, k)
 
 
 #  commitment scheme
@@ -121,12 +93,6 @@ def sharedCommit(coeffsS, coeffsT, k):
 def verifySecret(s, t, sharedCommit, i):
     currCommit = commitment(s, t) % q
     combCommit = (sharedCommit[0] * (power(sharedCommit[1], i, q))) % q
-    # combCommit = 1
-    # for j in range(1, len(sharedCommit)):
-    #     if j == 0:
-    #         combCommit *= sharedCommit[j]
-    #     else:
-    #         combCommit = combCommit * power(sharedCommit[j], power(i, j))
     print(currCommit, combCommit)
     return currCommit == (combCommit)
 
@@ -144,7 +110,8 @@ def reconstructSecret(shares, k):
 
 if __name__ == "__main__":
     # calc p,q
-    p, q = getPrimes(16)
+    p= 2875240349 
+    q = 136319
     # calc g
     g = getGenerators(q)
     # cal h
@@ -153,7 +120,7 @@ if __name__ == "__main__":
     print(p, q, g, h)
     print("\n")
 
-    secret = random.randrange(1, q)
+    secret = random.getrandbits(128)
     print("The secret is: " + str(secret))
     print("Number of shareholders: 2")
     shareno = 2
@@ -162,13 +129,12 @@ if __name__ == "__main__":
 
     # coefficient of s and t
     coeffsS = coeff(k, secret)
-    coeffsT = coeff(k, random.randrange(0, q))
+    coeffsT = coeff(k, random.getrandbits(128))
 
     # generate shares
-    shares = generateShares(coeffsS, coeffsT, shareno, k)
+    s,t = generateShares(coeffsS, coeffsT, shareno, k)
     print("Shares are:")
-    for share in shares:
-        print(share)
+    print(s,t)
     print("\n")
 
     # genrate public commitments
@@ -178,12 +144,5 @@ if __name__ == "__main__":
     print("\n")
 
     # verify shares
-    for i in range(shareno):
-        print("Verification for shareholder no. " + str(i+1) + ": ",
-              verifySecret(shares[i][0], shares[i][1], sharedCommits, i+1))
+    print("Verification for shares: ",verifySecret(s, t, sharedCommits, 1))
     print("\n")
-
-    # reconstruct secret using k shares
-    # kshares = random.sample(shares, k)
-    gensecret = reconstructSecret(shares, k)
-    print("Generated secret from 2 shares is: ", gensecret)
--- shamir.py
+++ shamir.py
@@ -1,17 +1,17 @@
 import random
 from math import ceil
 from decimal import Decimal
+from Crypto.Util import number
 
-global randsize
-randsize = 10**5
-_PRIME = 2 ** 127 - 1
+global randsize, p
+p = number.getPrime(64)
 
 
 # calc random coeficient
 def coeff(k, secret):
     coeff = [secret]
     for i in range(k-1):
-        coeff.append(random.randrange(0, randsize))
+        coeff.append(random.randrange(0, p-1))
     return coeff
 
 
@@ -28,34 +28,32 @@ def generateShares(secret, shareno, k):
     shares = []
     coeffs = coeff(k, secret)
     for x in range(1, shareno+1):
-        shares.append([x, calcY(x, coeffs, k) % _PRIME])
+        shares.append([x, calcY(x, coeffs, k) % p])
     return shares
 
 
 # reconstruct secret
 def reconstructSecret(shares):
-    gensecret = 0
-    prod = Decimal(1)
+    x0, y0 = shares[0][0], shares[0][1]
+    x1, y1 = shares[1][0], shares[1][1]
 
-    for i in range(len(shares)):
-        xi, yi = shares[i][0], shares[i][1]
-        prod = Decimal(1)
-        for j in range(len(shares)):
-            xj = shares[j][0]
-            if i != j:
-                prod *= Decimal(Decimal(xj)/(xj-xi)) % _PRIME
-        prod *= yi
-        gensecret += prod
-    return int(round(Decimal(gensecret), 0))
+    numerator = ((x1 * y0) - (x0 * y1)) % p
+    denominator = (x1 - x0) % p
+    gensecret = numerator // denominator
+    return gensecret
 
 
 if __name__ == "__main__":
-    print("Enter the secret:")
-    secret = int(input())
-    print("Enter number of shareholders:")
-    shareno = int(input())
-    print("Enter the value of k:")
-    k = int(input())
+    # value of p
+    print("Value of p is: " + str(p))
+    print("\n")
+
+    secret = random.randrange(1, p-1)
+    print("The secret is: " + str(secret))
+    print("Number of shareholders: 2")
+    shareno = 2
+    print("The value of k: 2")
+    k = 2
 
     # generate shares
     shares = generateShares(secret, shareno, k)
@@ -66,4 +64,5 @@ if __name__ == "__main__":
     # reconstruct secret using k shares
     kshares = random.sample(shares, k)
     gensecret = reconstructSecret(kshares)
-    print("Generated secret is: ", gensecret)
+    print("diff: ", str(gensecret-secret))
+    print("Generated secret from 2 shares is: ", gensecret)
